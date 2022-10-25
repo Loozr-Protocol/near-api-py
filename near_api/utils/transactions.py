83c695eae9fea51edd2ec6b29971e76a094b4c97
Confidence Yobo <confiyobo@gmail.com>
2022-10-25 18:26:17 +0100

    raise exception if jsonprovider error is not invalid nonce

--- near_api/utils/transactions.py
+++ near_api/utils/transactions.py
@@ -15,6 +15,9 @@ def tx_retry_with_cool_off(callback, receiver_id: str,
             result = callback(receiver_id, actions)
             return result
         except JsonProviderError as err:
+            if not err.is_invalid_nonce_tx_error() and not err.is_expired_tx_error():
+                raise err
+
             time.sleep(delay_time)
             delay_time *= cool_of_factor
     return None
